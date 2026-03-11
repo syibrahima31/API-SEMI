54821386ae5c53b5c7fed042168f4d9354f122ba
Ibrahima Sy <syibrahima31@gmail.com>
2026-03-11 20:50:31 +0000

    +++

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask 
+from flask import Flask, render_template 
+from data import eleves
 
 app = Flask(__name__)
 
 
+# creation du route principale qui est le point entre de app 
 @app.route("/")
 def index(): 
-    return "<h1> Bienvenue dans API de Flask-SEMI </h1>"
+    return render_template("index.html")
+
+# creation dune route eleves qui affiche des donnes 
+@app.route("/eleves")
+def list_eleves(): 
+    return render_template("list_eleves.html", students=eleves)
+
+
 
 if __name__ == "__main__": 
     app.run(debug=True )
